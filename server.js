81d6aee95245ef4c9882d210fe5a320eda8c5dca
Chuck Kang <chuckkang@gmail.com>
2017-10-06 08:36:52 -0700

    epic button

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var path = require("path");
+var app = express();
+
+app.use(session({ secret: 'codingdojorocks' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./static")));
+//setup views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+/////////////// routes ///////////////////
+app.get('/', function (req, res) {
+	res.render("index");
+})
+app.post('/', function (req, res) {
+	res.render("index");
+})
+
+///////////////// Socket data ///////////////////////
+var server = app.listen(8000, function(){
+	console.log("listening on port 8000.")
+})
+var io = require('socket.io').listen(server);
+var count = 0;	
+io.sockets.on('connection', function (socket) {
+/////////////////
+console.log("Client/socket is connected!");
+console.log("Client/socket id is: ", socket.id);
+// all the server socket code goes in here
+	
+socket.on("count", function (data) {
+	/////////////////
+	if (data.reset===true)
+		{count=0;}
+	else{ count++; }
+	
+	console.log(count, "----This is the counter")
+	socket.emit('counter', {
+		counter: count
+	});
+	// broadcast to everyoe
+	socket.broadcast.emit('counter', {
+		counter: count
+	});
+	/////////////////
+})
+/////////////////
+})
